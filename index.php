5e3f9e9a872f027e348837616e7da8fd94e1226c
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-08-23 09:45:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta chatset="utf-8">
+   <link ref="stylesheet" href="main.css">
+</head>
+<body>
+
+<?php
+$txt = "PHP";
+echo "My $txt website!";
+?>
+<p>This is just a test project of all i have learned</p>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+Name: <input type="text" name="name">
+<span class="error">* <?php echo $nameErr;?></span>
+<br><br>
+E-mail:
+<input type="text" name="email">
+<span class="error">* <?php echo $emailErr;?></span>
+<br><br>
+Website:
+<input type="text" name="website">
+<span class="error"><?php echo $websiteErr;?></span>
+<br><br>
+Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+<br><br>
+Gender:
+<input type="radio" name="gender" value="female">Female
+<input type="radio" name="gender" value="male">Male
+<input type="radio" name="gender" value="other">Other
+<span class="error">* <?php echo $genderErr;?></span>
+<br><br>
+<input type="submit" name="submit" value="Submit">
+
+</form>
+<?php
+ if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name required";
+  } else {
+    $name = test_input($_POST["name"]);
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+  }
+
+  if (empty($_POST["website"])) {
+    $website = "";
+  } else {
+    $website = test_input($_POST["website"]);
+  }
+
+  if (empty($_POST["comment"])) {
+    $comment = "";
+  } else {
+    $comment = test_input($_POST["comment"]);
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Input Gender please";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+}  
+   
+   
+?>
+</body>
+</html>
 
